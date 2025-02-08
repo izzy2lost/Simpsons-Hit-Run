c4cb40fb3ebc5ffd13ade05709d9a8d70c66dc04
3UR <76826837+3UR@users.noreply.github.com>
2025-02-08 19:01:04 +1000

    hack

--- src/libs/pure3d/pddi/gl/display_win32/gl.hpp
+++ src/libs/pure3d/pddi/gl/display_win32/gl.hpp
@@ -8,8 +8,12 @@
 #include <glad/glad.h>
 
 // TODO(3ur): this is a hack (idk why it isnt defined)
-#ifndef GL_EXT_separate_specular_color
-#define GL_LIGHT_MODEL_COLOR_CONTROL_EXT  0x81F8
-#define GL_SINGLE_COLOR_EXT               0x81F9
-#define GL_SEPARATE_SPECULAR_COLOR_EXT    0x81FA
-#endif // GL_EXT_separate_specular_color
+#define HACK
+
+#ifdef HACK
+#define GL_LIGHT_MODEL_COLOR_CONTROL_EXT 0x81F8
+#define GL_SINGLE_COLOR_EXT              0x81F9
+#define GL_SEPARATE_SPECULAR_COLOR_EXT   0x81FA
+#define GL_BGR_EXT                       0x80E0
+#define GL_BGRA_EXT                      0x80E1
+#endif
