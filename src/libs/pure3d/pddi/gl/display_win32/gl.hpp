ba14952f76d8a559c91d605328fe51f5a333cf0e
3UR <76826837+3UR@users.noreply.github.com>
2025-02-08 18:57:46 +1000

    hack

--- src/libs/pure3d/pddi/gl/display_win32/gl.hpp
+++ src/libs/pure3d/pddi/gl/display_win32/gl.hpp
@@ -6,3 +6,10 @@
 // stub OpenGL header, all pddi gl code uses this instead of '#include <GL/gl.h>
 
 #include <glad/glad.h>
+
+// TODO(3ur): this is a hack (idk why it isnt defined)
+#ifndef GL_EXT_separate_specular_color
+#define GL_LIGHT_MODEL_COLOR_CONTROL_EXT  0x81F8
+#define GL_SINGLE_COLOR_EXT               0x81F9
+#define GL_SEPARATE_SPECULAR_COLOR_EXT    0x81FA
+#endif // GL_EXT_separate_specular_color
